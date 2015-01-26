865a0b266b637ee46c0d5271bdd589e81976ae30
Maxim Gekk <gekk.maxim@huawei.com>
2015-01-26 14:46:13 +0300

    Updating of SparkDsl by adding of SparkConfsDsl

--- src/main/scala/scalan/spark/Spark.scala
+++ src/main/scala/scalan/spark/Spark.scala
@@ -1,16 +1,19 @@
 package scalan.spark
 
 trait SparkDsl extends SparkContextsDsl
+with SparkConfsDsl
 with RDDsDsl
 with PairRDDsDsl
 with BroadcastsDsl
 
 trait SparkDslSeq extends  SparkContextsDslSeq
+with SparkConfsDslSeq
 with RDDsDslSeq
 with PairRDDsDslSeq
 with BroadcastsDslSeq
 
 trait SparkDslExp extends SparkContextsDslExp
+with SparkConfsDslExp
 with RDDsDslExp
 with PairRDDsDslExp
 with BroadcastsDslExp
