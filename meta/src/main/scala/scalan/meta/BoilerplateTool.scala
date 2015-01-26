7c7a54762ef884365b26d22ed1cb21f99d970f69
Maxim Gekk <gekk.maxim@huawei.com>
2015-01-26 15:17:55 +0300

    Adding of SparkBoilerplateTool

--- meta/src/main/scala/scalan/meta/BoilerplateTool.scala
+++ meta/src/main/scala/scalan/meta/BoilerplateTool.scala
@@ -1,24 +1,31 @@
 package scalan.meta
 
-object StarterBoilerplateTool extends BoilerplateTool {
-  val starterTypeSynonims = Set(
-    "My"
-    // declare your type synonims for User Defined types here (see type PA[A] = Rep[PArray[A]])
+object SparkBoilerplateTool extends BoilerplateTool {
+  val sparkTypeSynonims = Map(
+    "RepSparkConf" -> "SparkConf",
+    "RepSparkContext" -> "SparkContext",
+    "RepRDD" -> "RDD",
+    "RepPairRDD" -> "PairRDDFunctions",
+    "RepBroadcast" -> "Broadcast"
   )
-  lazy val starterConfig = CodegenConfig(
-    srcPath = "src/main/scala",
+  lazy val sparkConfig = CodegenConfig(
+    srcPath = "src/main/scala/scalan/spark",
     entityFiles = List(
-      "scalan/examples/MyArrays.scala"
+      "SparkConf.scala",
+      "SparkContext.scala",
+      "RDD.scala",
+      "PairRDD.scala",
+      "Broadcast.scala"
     ),
     seqContextTrait = "ScalanSeq",
     stagedContextTrait = "ScalanExp",
     extraImports = List(
       "scala.reflect.runtime.universe._",
       "scalan.common.Default"),
-    starterTypeSynonims
+    sparkTypeSynonims
   )
 
-  override def getConfigs(args: Array[String]) = Seq(starterConfig)
+  override def getConfigs(args: Array[String]) = Seq(sparkConfig)
 
   override def main(args: Array[String]) = super.main(args)
 }
