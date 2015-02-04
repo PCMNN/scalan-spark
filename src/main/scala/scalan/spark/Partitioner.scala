39675e1094891000ebcabb11523b1a95d4ae5654
Maxim Gekk <gekk.maxim@huawei.com>
2015-02-04 11:47:02 +0300

    Partitioner is removed because it is not used.

--- src/main/scala/scalan/spark/Partitioner.scala
+++
@@ -1,9 +0,0 @@
-package scalan.spark
-
-trait Partitioners {
-  /** The trait defines how the elements in a key-value pair RDD are partitioned by key.
-   * They maps each key to a partition ID, from 0 to `numPartitions - 1`. **/
-  trait SPartitioner
-
-  trait SPartitionerCompanion
-}
