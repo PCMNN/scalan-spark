1af6554721acbe6726f7c0b88dbc7d82c82284a0
Maxim Klinov <klinov.maxim@huawei.com>
2015-09-03 17:34:09 +0300

    TRON LR train works, seems slow

--- spark-backend/src/test/resources/com/scalan/spark/TronSparkTrain/run.scala
+++ spark-backend/src/test/resources/com/scalan/spark/TronSparkTrain/run.scala
@@ -91,12 +91,11 @@ object run {
     outExp should equal (outSeq)
   }
   */
-  // type ParametersPaired = (Int, (Double, (Double, (Double, (Double, (Double, (Int, Double)))))))
 
-  // (org.apache.spark.rdd.RDD[(Long, Array[Int])], (org.apache.spark.rdd.RDD[(Long, Array[Double])], (Int,
-  //   (org.apache.spark.rdd.RDD[(Long, Double)], (Int, (Int, (Double, (Double, Double)))))))))
   def main(args: Array[String]): Unit = {
+
     val timeStart = System.currentTimeMillis()
+
     if (args.length != 5) {
       System.out.println("Usage: <host:string> <inputPath:string> <split:string> <eps:int> <outputPath:string>")
       System.exit(0)
@@ -107,7 +106,6 @@ object run {
     val eps = args(3).toDouble
     val outputPath = args(4)
     val b = 1.0    // should not be changed
-
     val globalSparkConf = new SparkConf()
       .setAppName("HiTronSparkTrain")
       .setMaster(host)
@@ -115,39 +113,38 @@ object run {
         "-XX:+UseG1GC -XX:ParallelGCThreads=12 -XX:ConcGCThreads=5 -XX:+UseCompressedOops")
     globalSparkContext = new SparkContext(globalSparkConf)
 
-    val timeBeforeInput = System.currentTimeMillis()
+    val timeAfterContext = System.currentTimeMillis()
 
     val input = trainValues(host, inputPath, split, eps, b)
+    val (rddIdxsPartitioned, (rddValsPartitioned, (_, (rddVYPartitioned, _)))) = input
+    rddIdxsPartitioned.cache
+    rddValsPartitioned.cache
+    rddVYPartitioned.cache
+    val count = rddIdxsPartitioned.count + rddValsPartitioned.count + rddVYPartitioned.count  // to measure IO time
 
-    //  val (rddA, rddB) = (input._2._1, input._2._2._1)
-    //  val count = rddA.count + rddB.count
-    //
-    //  val timeAfterInput = System.currentTimeMillis()
-    //
-    val trainRes = execTrain(input)
-    println(s"res = ${trainRes.toList}")
-    //  val (mu, (vBu, (vBi, (mP, (mQ, (mY, (iters, (err, (flag, (stepDecrease)))))))))) = trainRes
-    //  println("Train finished. sqrtErr: " + err)
-    //
-    //  val timeBeforeOutput = System.currentTimeMillis()
-    //
-    //  val model: testUtil.Model = (nItems, (mu, (vBu, (vBi, (mP, (mQ, mY))))))
+    val timeAfterInput = System.currentTimeMillis()
+
+    val model = execTrain(input)
+    println(s"res = ${model.map(x => f"$x%.6f").toList}")
+
+    val timeAfterComputation = System.currentTimeMillis()
+
     //  testUtil.Result(model).saveModel(outputPath)
     //
     //  val timeEnd = System.currentTimeMillis()
-    //
-    //  val times = List(timeStart, timeBeforeInput, timeAfterInput, timeBeforeOutput, timeEnd)
-    //  val timeIntervals = (times.drop(1) zip times.dropRight(1)).map(p => (p._1 - p._2) / 1000.0)
-    //
-    //  val timeSparkContext = timeIntervals(0)
-    //  val timeIo = timeIntervals(1) + timeIntervals(3)
-    //  val timeComputation = timeIntervals(2)
-    //  val timeTotal = (timeEnd - timeStart) / 1000.0
-    //
-    //  println(f"SparkContext time = $timeSparkContext%.3f sec")
-    //  println(f"SVDPlusPlus io time = $timeIo%.3f sec")
-    //  println(f"SVDPlusPlus computation time = $timeComputation%.3f sec")
-    //  println(f"SVDPlusPlus total time = $timeTotal%.3f sec")
+
+    val times = List(timeStart, timeAfterContext, timeAfterInput, timeAfterComputation) //, timeEnd)
+    val timeIntervals = (times.drop(1) zip times.dropRight(1)).map(p => (p._1 - p._2) / 1000.0)
+
+    val timeSparkContext = timeIntervals(0)
+    val timeIo = timeIntervals(1) // + timeIntervals(3)
+    val timeComputation = timeIntervals(2)
+    val timeTotal = timeSparkContext + timeIo + timeComputation
+
+    println(f"SparkContext time = $timeSparkContext%.3f sec")
+    println(f"TRON LR io time = $timeIo%.3f sec")
+    println(f"TRON LR computation time = $timeComputation%.3f sec")
+    println(f"TRON LR total time = $timeTotal%.3f sec")
 
     stop()
   }
@@ -166,29 +163,12 @@ object run {
 
   def trainValues(host: String, inputPath: String, split: String, eps: Double, b: Double) = {
 
-    //  val (_, arrTrainIndices, arrTrainValues) = LoadMovieLensData_IntoRatingMatrix(0.0, 1.0, inputPath, -1.0, nUsers, nItems, split)
-
     {
       // This is a hack in order to initialize default parallelism correctly
       val tmp = globalSparkContext.makeRDD(Array(1))
       tmp.count
     }
-    //  val params = (maxIterations, (delta, (gamma1, (gamma2, (lambda6, (lambda7, (width, stepDecrease)))))))
-    //
-    //  val rddIdxs = globalSparkContext.makeRDD(arrTrainIndices.indices.map { _.toLong} zip arrTrainIndices, globalSparkContext.defaultParallelism)
-    //  val rddIdxsPartitioned = (new org.apache.spark.rdd.OrderedRDDFunctions[Long, Array[Int], (Long, Array[Int])](rddIdxs)).repartitionAndSortWithinPartitions(
-    //   defaultPartitioner(globalSparkContext.defaultParallelism)).cache
-    //
-    //  val rddVals = globalSparkContext.makeRDD(arrTrainValues.indices.map {_.toLong} zip arrTrainValues, globalSparkContext.defaultParallelism)
-    //  val rddValsPartitioned = (new org.apache.spark.rdd.OrderedRDDFunctions[Long, Array[Double], (Long, Array[Double])](rddVals)).repartitionAndSortWithinPartitions(
-    //   defaultPartitioner(globalSparkContext.defaultParallelism)).cache
-    //
-    //  val res = (params, (rddIdxsPartitioned, (rddValsPartitioned, (nItems, stddev))))
-    //  //rddIdxsPartitioned.unpersist()
-    //  //rddValsPartitioned.unpersist()
-    //  res
 
-    // usually used split = ":"
     def loadLibSVMData(sc: SparkContext, path: String, numPartitions: Int, split: String, b: Double): (RDD[(Long, Array[Int], Array[Double], Double)], Int) = {
       val parsed = sc.textFile(path, numPartitions).map(_.trim).filter(!_.isEmpty)
       val parsedWithLineIds = parsed.zipWithIndex.mapPartitions(block => block.map { case (line, id) => (id, line) })
@@ -208,18 +188,17 @@ object run {
       val res = if (isBias) {
         val maxIdBc = sc.broadcast(maxId)
         val dataRes = data.mapPartitions { blocks => blocks.map { case (id, indexes, values, y) =>
-          (id, indexes :+ maxIdBc.value + 1, values :+ b, y)
+          (id, indexes :+ (maxIdBc.value + 1), values :+ b, y)
         }}
-        (dataRes, maxId + 1)
+        (dataRes, maxId + 2)
       } else {
-        (data, maxId)
+        (data, maxId + 1)
       }
       res
     }
 
-    val (data, maxId) = loadLibSVMData(globalSparkContext, inputPath, 2, split, b)
-    println(s"load instances = ${data.collect.length}")
-    println(s"load values = ${val r = data.collect.apply(0); (r._2.toList, r._3.toList, r._4)}, nColumnsMatrix = $maxId")
+    val numPartitions = 2
+    val (data, nColumnsMatrix) = loadLibSVMData(globalSparkContext, inputPath, numPartitions, split, b)
 
     val rddIdxs = data.mapPartitions { blocks => blocks.map { case d => (d._1, d._2) } }
     val rddIdxsPartitioned = (new org.apache.spark.rdd.OrderedRDDFunctions[Long, Array[Int], (Long, Array[Int])](rddIdxs)).repartitionAndSortWithinPartitions(
@@ -229,8 +208,6 @@ object run {
     val rddValsPartitioned = (new org.apache.spark.rdd.OrderedRDDFunctions[Long, Array[Double], (Long, Array[Double])](rddVals)).repartitionAndSortWithinPartitions(
       defaultPartitioner(globalSparkContext.defaultParallelism)).cache
 
-    val nColumnsMatrix = maxId
-
     val rddVY = data.mapPartitions { blocks => blocks.map { case d => (d._1, d._4) } }
     val rddVYPartitioned = (new org.apache.spark.rdd.OrderedRDDFunctions[Long, Double, (Long, Double)](rddVY)).repartitionAndSortWithinPartitions(
       defaultPartitioner(globalSparkContext.defaultParallelism)).cache
@@ -239,40 +216,5 @@ object run {
     val res = (rddIdxsPartitioned, (rddValsPartitioned, (nColumnsMatrix, (rddVYPartitioned, params))))
     res
   }
-
-  //  def LoadMovieLensData_IntoRatingMatrix(start: Double, end: Double, rawDataPath: String, outputMinValue: Double, maxUsers: Int, maxItems: Int, split: String):
-  //  (Int, Array[Array[Int]], Array[Array[Double]]) = {
-  //    val lines = globalSparkContext.textFile(rawDataPath, 1).collect
-  //    val lengthItems: Array[Int] = new Array[Int](maxUsers)
-  //    for (line1 <- lines) {
-  //      val line = line1.trim()
-  //      val tokens = line.split(split)
-  //      val user = tokens(0).toInt - 1
-  //      if ((user >= 0) && (user < maxUsers)) lengthItems(user) = lengthItems(user) + 1
-  //    }
-  //    val resultXvals: Array[Array[Double]] = new Array[Array[Double]](maxUsers)
-  //    val resultXidxs: Array[Array[Int]] = new Array[Array[Int]](maxUsers)
-  //    for (i <- 0 until maxUsers) {
-  //      resultXvals(i) = new Array[Double](lengthItems(i))
-  //      resultXidxs(i) = new Array[Int](lengthItems(i))
-  //      lengthItems(i) = 0
-  //    }
-  //    for (line1 <- lines) {
-  //      val line = line1.trim()
-  //      val tokens = line.split(split)
-  //      val i = tokens(0).toInt - 1
-  //      val j = tokens(1).toInt - 1
-  //      val r = tokens(2).toDouble
-  //      resultXvals(i)(lengthItems(i)) = r
-  //      resultXidxs(i)(lengthItems(i)) = j
-  //      if ((i >= 0) && (i < maxUsers)) lengthItems(i) = lengthItems(i) + 1
-  //    }
-  //    val a = (resultXidxs zip resultXvals).map(v => v._1 zip v._2)
-  //    val b = a.map(v => v.sortWith(_._1 < _._1))
-  //    val c = b.map(v => v.unzip).map(v => (v._1.toArray, v._2.toArray)).unzip
-  //    val iss: Array[Array[Int]] = c._1.toArray
-  //    val vss: Array[Array[Double]] = c._2.toArray
-  //    (maxItems, iss, vss)
-  //  }
 }
 
