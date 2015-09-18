cad87333042f11ba6eb499546f183220be03e3aa
Maxim Klinov <klinov.maxim@huawei.com>
2015-09-18 15:12:18 +0300

    use for arrayRandomGaussian SparkLms version

--- spark-backend/src/main/scala/com/scalan/spark/backend/SparkArrayOpsExp.scala
+++ spark-backend/src/main/scala/com/scalan/spark/backend/SparkArrayOpsExp.scala
@@ -11,14 +11,14 @@ import scalan.compilation.lms.cxx.sharedptr.CxxShptrCodegen
 
 trait SparkArrayOpsExp extends Variables with Transforming with VariablesExp { self: LmsBackendFacade =>
 
-  case class ArrayRandomGaussian[A:Manifest](a: Exp[Double], e: Exp[Double], xs: Exp[Array[Double]]) extends Def[Array[Double]] {
+  case class ArrayRandomGaussianSparkLms[A:Manifest](a: Exp[Double], e: Exp[Double], xs: Exp[Array[Double]]) extends Def[Array[Double]] {
     val m = manifest[Double]
   }
 
-  def array_randomGaussian[A:Manifest](a: Rep[Double], e: Rep[Double], arr: Rep[Array[Double]]): Rep[Array[Double]] = ArrayRandomGaussian(a, e, arr)
+  def array_randomGaussianSparkLms[A:Manifest](a: Rep[Double], e: Rep[Double], arr: Rep[Array[Double]]): Rep[Array[Double]] = ArrayRandomGaussianSparkLms(a, e, arr)
 
   override def mirror[A:Manifest](e: Def[A], f: Transformer)(implicit pos: SourceContext): Exp[A] = (e match {
-    case ArrayRandomGaussian(a, d, arr) => array_randomGaussian(f(a), f(d), f(arr))(mtype(manifest[Double])).asInstanceOf[Exp[A]]
+    case ArrayRandomGaussianSparkLms(a, d, arr) => array_randomGaussianSparkLms(f(a), f(d), f(arr))(mtype(manifest[Double])).asInstanceOf[Exp[A]]
     case _ => super.mirror(e,f)
   }).asInstanceOf[Exp[A]]
 }
@@ -28,7 +28,7 @@ trait SparkScalaGenArrayOps extends ScalaGenEffect {
   import IR._
 
   override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
-    case ds @ ArrayRandomGaussian(a, d, arr) =>
+    case ds @ ArrayRandomGaussianSparkLms(a, d, arr) =>
       // TODO use proper source quasiquoter
       stream.println("// generating Random Gaussian values in array")
       stream.println("val " + quote(sym) + " = " + quote(arr) + ".map { _ => util.Random.nextGaussian() * " +
